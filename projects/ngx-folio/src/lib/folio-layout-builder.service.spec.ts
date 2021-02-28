444f4d5db547e6b692172980b6858b091004b73e
dmitrii <ffklffkl@gmail.com>
2021-03-01 00:03:01 +0200

    test(folio-layout-builder): grouping tests

--- projects/ngx-folio/src/lib/folio-layout-builder.service.spec.ts
+++ projects/ngx-folio/src/lib/folio-layout-builder.service.spec.ts
@@ -1,170 +1,176 @@
 import { FolioLayoutBuilderOptions, FolioLayoutBuilderService } from './folio-layout-builder.service';
 
 describe('FolioLayoutBuilderService', () => {
-  it('should build layout without cursor segment and hidden pages', () => {
-    const options: FolioLayoutBuilderOptions = {
-      currentPage: 1,
-      maxPage: 4,
-      startSegmentMax: 2,
-      endSegmentMax: 2,
-      cursorSegmentMax: 3,
-    };
-    const builder = new FolioLayoutBuilderService();
+  describe('when the sum of segments is less then or equal to maxPage', () => {
+    it('should build layout without cursor segment', () => {
+      const options: FolioLayoutBuilderOptions = {
+        currentPage: 1,
+        maxPage: 4,
+        startSegmentMax: 2,
+        endSegmentMax: 2,
+        cursorSegmentMax: 3,
+      };
+      const builder = new FolioLayoutBuilderService();
 
-    const layout = builder.createLayout(options);
+      const layout = builder.createLayout(options);
 
-    expect(layout).toEqual([1, 2, 3, 4]);
+      expect(layout).toEqual([1, 2, 3, 4]);
+    });
+
+    it('should build layout with cursor segment', () => {
+      const options: FolioLayoutBuilderOptions = {
+        currentPage: 1,
+        maxPage: 7,
+        startSegmentMax: 2,
+        endSegmentMax: 2,
+        cursorSegmentMax: 3,
+      };
+      const builder = new FolioLayoutBuilderService();
+
+      const layout = builder.createLayout(options);
+
+      expect(layout).toEqual([1, 2, 3, 4, 5, 6, 7]);
+    });
   });
 
-  it('should build layout without hidden pages for maxPage equals to sum of segments', () => {
-    const options: FolioLayoutBuilderOptions = {
-      currentPage: 1,
-      maxPage: 7,
-      startSegmentMax: 2,
-      endSegmentMax: 2,
-      cursorSegmentMax: 3,
-    };
-    const builder = new FolioLayoutBuilderService();
+  describe('when the sum of segments is more by one than maxPage', () => {
+    it('should build layout with coupled start and end segments when currentPage is inside start segment', () => {
+      const options: FolioLayoutBuilderOptions = {
+        currentPage: 1,
+        maxPage: 8,
+        startSegmentMax: 2,
+        endSegmentMax: 2,
+        cursorSegmentMax: 3,
+      };
+      const builder = new FolioLayoutBuilderService();
 
-    const layout = builder.createLayout(options);
+      const layout = builder.createLayout(options);
 
-    expect(layout).toEqual([1, 2, 3, 4, 5, 6, 7]);
+      // TODO: rework to [1, 2, 3, 4, 5, '...', 7, 8]
+      expect(layout).toEqual([1, 2, '...', 4, 5, 6, 7, 8]);
+    });
+
+    it('should build layout with coupled cursor and end segments when currentPage is inside end segment', () => {
+      const options: FolioLayoutBuilderOptions = {
+        currentPage: 8,
+        maxPage: 8,
+        startSegmentMax: 2,
+        endSegmentMax: 2,
+        cursorSegmentMax: 3,
+      };
+      const builder = new FolioLayoutBuilderService();
+
+      const layout = builder.createLayout(options);
+
+      expect(layout).toEqual([1, 2, '...', 4, 5, 6, 7, 8]);
+    });
   });
 
-  it('should build layout with one hidden page and cursor segment joined to start segment when current page is in first segment', () => {
-    const options: FolioLayoutBuilderOptions = {
-      currentPage: 1,
-      maxPage: 8,
-      startSegmentMax: 2,
-      endSegmentMax: 2,
-      cursorSegmentMax: 3,
-    };
-    const builder = new FolioLayoutBuilderService();
+  describe('when difference between maxPage and the sum of segments is more than one', () => {
+    it('should build layout with ellipsis between segments', () => {
+      const options: FolioLayoutBuilderOptions = {
+        currentPage: 1,
+        maxPage: 9,
+        startSegmentMax: 2,
+        endSegmentMax: 2,
+        cursorSegmentMax: 3,
+      };
+      const builder = new FolioLayoutBuilderService();
 
-    const layout = builder.createLayout(options);
+      const layout = builder.createLayout(options);
 
-    // TODO: rework to [1, 2, 3, 4, 5, '...', 7, 8]
-    expect(layout).toEqual([1, 2, '...', 4, 5, 6, 7, 8]);
-  });
+      expect(layout).toEqual([1, 2, '...', 4, 5, 6, '...', 8, 9]);
+    });
 
-  it('should build layout with one hidden page and cursor segment joined to end segment when current page is in end segment', () => {
-    const options: FolioLayoutBuilderOptions = {
-      currentPage: 8,
-      maxPage: 8,
-      startSegmentMax: 2,
-      endSegmentMax: 2,
-      cursorSegmentMax: 3,
-    };
-    const builder = new FolioLayoutBuilderService();
+    it('should build coupled start and cursor segments when current page is right after start segment', () => {
+      const options: FolioLayoutBuilderOptions = {
+        currentPage: 3,
+        maxPage: 9,
+        startSegmentMax: 2,
+        endSegmentMax: 2,
+        cursorSegmentMax: 3,
+      };
+      const builder = new FolioLayoutBuilderService();
 
-    const layout = builder.createLayout(options);
+      const layout = builder.createLayout(options);
 
-    expect(layout).toEqual([1, 2, '...', 4, 5, 6, 7, 8]);
-  });
+      expect(layout).toEqual([1, 2, 3, 4, 5, '...', 8, 9]);
+    });
 
-  it('should build layout with two hidden pages', () => {
-    const options: FolioLayoutBuilderOptions = {
-      currentPage: 1,
-      maxPage: 9,
-      startSegmentMax: 2,
-      endSegmentMax: 2,
-      cursorSegmentMax: 3,
-    };
-    const builder = new FolioLayoutBuilderService();
+    it('should build cupled end and cursor segments when current page is just before end segment', () => {
+      const options: FolioLayoutBuilderOptions = {
+        currentPage: 7,
+        maxPage: 9,
+        startSegmentMax: 2,
+        endSegmentMax: 2,
+        cursorSegmentMax: 3,
+      };
+      const builder = new FolioLayoutBuilderService();
 
-    const layout = builder.createLayout(options);
+      const layout = builder.createLayout(options);
 
-    expect(layout).toEqual([1, 2, '...', 4, 5, 6, '...', 8, 9]);
-  });
+      expect(layout).toEqual([1, 2, '...', 5, 6, 7, 8, 9]);
+    });
 
-  it('should join start and cursor segments when current page is right after start segment', () => {
-    const options: FolioLayoutBuilderOptions = {
-      currentPage: 3,
-      maxPage: 9,
-      startSegmentMax: 2,
-      endSegmentMax: 2,
-      cursorSegmentMax: 3,
-    };
-    const builder = new FolioLayoutBuilderService();
+    it('should build cursor segment in the middle when maxPage is even and cursorSegment is odd', () => {
+      const options: FolioLayoutBuilderOptions = {
+        currentPage: 1,
+        maxPage: 10,
+        startSegmentMax: 2,
+        endSegmentMax: 2,
+        cursorSegmentMax: 3,
+      };
+      const builder = new FolioLayoutBuilderService();
 
-    const layout = builder.createLayout(options);
+      const layout = builder.createLayout(options);
 
-    expect(layout).toEqual([1, 2, 3, 4, 5, '...', 8, 9]);
-  });
+      // TODO: rework to [1, 2, '...', 4, 5, 6, '...', 9, 10]
+      expect(layout).toEqual([1, 2, '...', 5, 6, 7, '...', 9, 10]);
+    });
 
-  it('should join end and cursor segments when current page is just before end segment', () => {
-    const options: FolioLayoutBuilderOptions = {
-      currentPage: 7,
-      maxPage: 9,
-      startSegmentMax: 2,
-      endSegmentMax: 2,
-      cursorSegmentMax: 3,
-    };
-    const builder = new FolioLayoutBuilderService();
+    it('should build cursor segment in the middle when maxPage is odd and cursorSegment is odd', () => {
+      const options: FolioLayoutBuilderOptions = {
+        currentPage: 1,
+        maxPage: 11,
+        startSegmentMax: 2,
+        endSegmentMax: 2,
+        cursorSegmentMax: 3,
+      };
+      const builder = new FolioLayoutBuilderService();
 
-    const layout = builder.createLayout(options);
+      const layout = builder.createLayout(options);
 
-    expect(layout).toEqual([1, 2, '...', 5, 6, 7, 8, 9]);
-  });
+      expect(layout).toEqual([1, 2, '...', 5, 6, 7, '...', 10, 11]);
+    });
 
-  it('should build cursor segment right in the middle for even number of max page and odd number of cursor segment', () => {
-    const options: FolioLayoutBuilderOptions = {
-      currentPage: 1,
-      maxPage: 10,
-      startSegmentMax: 2,
-      endSegmentMax: 2,
-      cursorSegmentMax: 3,
-    };
-    const builder = new FolioLayoutBuilderService();
+    it('should build cursor segment in the middle when maxPage is odd and cursorSegment is even', () => {
+      const options: FolioLayoutBuilderOptions = {
+        currentPage: 1,
+        maxPage: 11,
+        startSegmentMax: 2,
+        endSegmentMax: 2,
+        cursorSegmentMax: 2,
+      };
+      const builder = new FolioLayoutBuilderService();
 
-    const layout = builder.createLayout(options);
+      const layout = builder.createLayout(options);
 
-    // TODO: rework to [1, 2, '...', 4, 5, 6, '...', 9, 10]
-    expect(layout).toEqual([1, 2, '...', 5, 6, 7, '...', 9, 10]);
-  });
+      expect(layout).toEqual([1, 2, '...', 5, 6, '...', 10, 11]);
+    });
 
-  it('should build cursor segment right in the middle for odd number of max page and odd number of cursor segment', () => {
-    const options: FolioLayoutBuilderOptions = {
-      currentPage: 1,
-      maxPage: 11,
-      startSegmentMax: 2,
-      endSegmentMax: 2,
-      cursorSegmentMax: 3,
-    };
-    const builder = new FolioLayoutBuilderService();
+    it('should build cursor segment in the middle when maxPage is even and cursorSegment is even', () => {
+      const options: FolioLayoutBuilderOptions = {
+        currentPage: 1,
+        maxPage: 10,
+        startSegmentMax: 2,
+        endSegmentMax: 2,
+        cursorSegmentMax: 2,
+      };
+      const builder = new FolioLayoutBuilderService();
 
-    const layout = builder.createLayout(options);
+      const layout = builder.createLayout(options);
 
-    expect(layout).toEqual([1, 2, '...', 5, 6, 7, '...', 10, 11]);
-  });
-
-  it('should build cursor segment right in the middle for odd number of max page and even number of cursor segment', () => {
-    const options: FolioLayoutBuilderOptions = {
-      currentPage: 1,
-      maxPage: 11,
-      startSegmentMax: 2,
-      endSegmentMax: 2,
-      cursorSegmentMax: 2,
-    };
-    const builder = new FolioLayoutBuilderService();
-
-    const layout = builder.createLayout(options);
-
-    expect(layout).toEqual([1, 2, '...', 5, 6, '...', 10, 11]);
-  });
-
-  it('should build cursor segment right in the middle for even number of max page and even number of cursor segment', () => {
-    const options: FolioLayoutBuilderOptions = {
-      currentPage: 1,
-      maxPage: 10,
-      startSegmentMax: 2,
-      endSegmentMax: 2,
-      cursorSegmentMax: 2,
-    };
-    const builder = new FolioLayoutBuilderService();
-
-    const layout = builder.createLayout(options);
-
-    expect(layout).toEqual([1, 2, '...', 5, 6, '...', 9, 10]);
+      expect(layout).toEqual([1, 2, '...', 5, 6, '...', 9, 10]);
+    });
   });
 });
